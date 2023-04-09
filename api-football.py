c333f84ec3ef89035bb103c92f8ad2a0e298e026
Rinji Le <rinji-le@hotmail.com>
2023-04-10 01:06:14 +0200

    Fixture stats implementation

--- api-football.py
+++ api-football.py
@@ -10,7 +10,6 @@ TODO
 
 import pandas as pd
 import requests
-import json
 import sys
 import os
 from tqdm import tqdm
@@ -18,10 +17,7 @@ import time
 
 URL = "https://v3.football.api-sports.io"
 API_KEY = list(open(".api_key"))[0].strip()
-HEADERS = {
-    "x-apisports-key": API_KEY
-}
-IDS = {}
+HEADERS = {"x-apisports-key": API_KEY}
 
 
 def read_ids(file):
@@ -41,6 +37,24 @@ def read_ids(file):
     return ids
 
 
+def check_file_exists(filename):
+    """
+    Checks if the file already exists. If it does, the user is asked if he/she
+    wants to overwrite the file.
+
+    :param filename: Name of the file (str).
+    :return: None.
+    """
+    if os.path.isfile(f"data/{filename}.json") or os.path.isfile(f"data/{filename}.csv"):
+        print("File already exists!")
+        while True:
+            answer = input("Do you want to overwrite the file? (y/n): ")
+            if answer == "y":
+                break
+            elif answer == "n":
+                sys.exit(0)
+
+
 def request(path, query):
     """
     Sends a get request to the API. The API has a limit of 300 requests per
@@ -56,19 +70,20 @@ def request(path, query):
     return response
 
 
-def get_fixtures(start_season=2015, end_season=2021):
+def get_fixtures(ids, start_season=2015, end_season=2021):
     """
     Gets the fixtures for all leagues in IDS for the given seasons.
 
+    :param ids: Dictionary with the ids and names of the leagues (dict).
     :param start_season: The first season to get the fixtures for (int).
     :param end_season: The last season to get the fixtures for (int).
     :return: All fixtures (pd.DataFrame).
     """
     fixtures = pd.DataFrame()
 
-    for league in tqdm(IDS.keys(), desc="Leagues"):
+    for league in tqdm(ids.keys(), desc="Leagues"):
         for season in tqdm(range(start_season, end_season + 1), leave=False, desc="Seasons"):
-            query = {"league": IDS[league], "season": season}
+            query = {"league": ids[league], "season": season}
             response = request("/fixtures", query).json()
 
             if response["errors"]:
@@ -80,38 +95,60 @@ def get_fixtures(start_season=2015, end_season=2021):
     return fixtures
 
 
-def save_fixtures(fixtures, filename):
+def save_data(data, filename):
     """
-    Saves the fixtures to a json and csv file.
+    Saves the data to a json and csv file.
 
-    :param fixtures: Dataframe with fixtures (pd.DataFrame).
+    :param data: Dataframe with the data (pd.DataFrame).
     :param filename: Name of the file (str).
     :return: None.
     """
-    fixtures.to_json(f"data/{filename}.json", orient="records", indent=4)
-    fixtures.to_csv(f"data/{filename}.csv", index=False)
+    data.to_json(f"data/{filename}.json", orient="records", indent=4)
+    data.to_csv(f"data/{filename}.csv", index=False)
+
+
+def get_fixture_stats():
+    stats = pd.DataFrame()
+
+    fixtures = pd.read_csv("data/fixtures.csv")
+    fixture_ids = fixtures["fixture.id"]
+
+    for idx in tqdm(fixture_ids, desc="Fixtures"):
+        query = {"fixture": idx}
+        response = request("/fixtures/statistics", query).json()
+
+        if response["errors"]:
+            print(f"Error (fixture {id}): {response['errors']}")
+        else:
+            new_stats = pd.json_normalize(response,
+                                          record_path=["response", "statistics"],
+                                          meta=[["parameters", "fixture"],
+                                                ["response", "team", "id"],
+                                                ["response", "team", "name"]])
+            stats = pd.concat([stats, new_stats])
+
+    return stats
 
 
 def main():
-    if len(sys.argv) == 1:
-        filename = "data"
+    if len(sys.argv) != 3:
+        print("Usage: python3 api-football.py -1/-2 <filename>")
+        sys.exit(1)
+
+    filename = sys.argv[2]
+    check_file_exists(filename)
+
+    ids = read_ids("ids.csv")
+
+    if sys.argv[1] == "-1":
+        data = get_fixtures(ids)
+    elif sys.argv[1] == "-2":
+        data = get_fixture_stats()
     else:
-        filename = sys.argv[1]
+        print("Usage: python3 api-football.py -1/-2 <filename>")
+        sys.exit(1)
 
-    if os.path.isfile(f"data/{filename}.json") or os.path.isfile(f"data/{filename}.csv"):
-        print("File already exists!")
-        while True:
-            answer = input("Do you want to overwrite the file? (y/n): ")
-            if answer == "y":
-                break
-            elif answer == "n":
-                sys.exit()
-
-    global IDS
-    IDS = read_ids("ids.csv")
-
-    fixtures = get_fixtures()
-    save_fixtures(fixtures, filename)
+    save_data(data, filename)
 
 
 if __name__ == "__main__":
