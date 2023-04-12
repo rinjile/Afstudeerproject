2fc5895e58fdaa0eb9f64398b9f1213361a29bb7
Rinji Le <rinji-le@hotmail.com>
2023-04-12 11:31:16 +0200

    Implemented multiple fixture stats for ml data

--- ml_data.py
+++ ml_data.py
@@ -33,7 +33,7 @@ def get_prev_fixtures(fixtures, fixture_id, team_id, n=1):
     return None
 
 
-def get_fixture_stats(fixture_stats, fixture_id, team_id, home):
+def get_fixture_stats(fixture_stats, fixture_id, team_id, description):
     stats = fixture_stats[
         (fixture_stats["fixture.id"] == fixture_id) &
         (fixture_stats["response.team.id"] == team_id)
@@ -51,34 +51,34 @@ def get_fixture_stats(fixture_stats, fixture_id, team_id, home):
     stats["Ball Possession"] = stats["Ball Possession"].str.replace("%", "").astype(float) / 100
     stats["Passes %"] = stats["Passes %"].str.replace("%", "").astype(float) / 100
 
-    if home:
-        stats.columns = [f"{col} (home)" for col in stats.columns]
-    else:
-        stats.columns = [f"{col} (away)" for col in stats.columns]
+    stats.columns = [f"{col} ({description})" for col in stats.columns]
 
     return stats
 
 
-def create_data_and_targets(fixtures, fixture_stats):
+def create_data_and_targets(fixtures, fixture_stats, n=1):
     # TODO: optimize?
     data = pd.DataFrame()
     targets = pd.Series(name="winner", dtype=int)
 
-    for (i, row) in tqdm(fixtures.iterrows(), desc="Creating data and targets", total=fixtures.shape[0]):
-        home = get_prev_fixtures(fixtures, row["fixture.id"], row["teams.home.id"])
-        away = get_prev_fixtures(fixtures, row["fixture.id"], row["teams.away.id"])
+    for (_, row) in tqdm(fixtures.iterrows(), desc="Creating data and targets", total=fixtures.shape[0]):
+        home = get_prev_fixtures(fixtures, row["fixture.id"], row["teams.home.id"], n=n)
+        away = get_prev_fixtures(fixtures, row["fixture.id"], row["teams.away.id"], n=n)
 
-        if home is None or away is None:
+        if home is None or away is None or home.size != n or away.size != n:
             continue
 
-        # TODO: als er meer dan 1 wedstrijd is
-        home_stats = get_fixture_stats(fixture_stats, home.iloc[0], row["teams.home.id"], True)
-        away_stats = get_fixture_stats(fixture_stats, away.iloc[0], row["teams.away.id"], False)
+        stats = pd.DataFrame()
 
-        if home_stats is None or away_stats is None:
-            continue
+        for i in range(n):
+            home_stats = get_fixture_stats(fixture_stats, home.iloc[i], row["teams.home.id"], f"home {i + 1}")
+            away_stats = get_fixture_stats(fixture_stats, away.iloc[i], row["teams.away.id"], f"away {i + 1}")
 
-        stats = pd.concat([home_stats, away_stats], axis=1)
+            if home_stats is None or away_stats is None:
+                continue
+
+            stats = pd.concat([stats, home_stats, away_stats], axis=1)
+
         data = pd.concat([data, stats], axis=0)
 
         if row["teams.home.winner"] is True:
@@ -100,10 +100,10 @@ def main():
     fixtures = fixtures[fixtures["fixture.status.short"].isin(["FT", "AET", "PEN"])]
     # fixtures = fixtures.head(500)
 
-    fixture_stats = pd.read_csv("data/fixture_stats1-7809.csv", low_memory=False)
+    fixture_stats = pd.read_csv("data/fixture_stats1-15652.csv", low_memory=False)
 
-    data, targets = create_data_and_targets(fixtures, fixture_stats)
-    data.to_csv("data/data.csv", index=False)
+    data, targets = create_data_and_targets(fixtures, fixture_stats, n=2)
+    data.to_csv("data/ml_data.csv", index=False)
     targets.to_csv("data/targets.csv", index=False, header=False)
 
     if ERROR:
