7420964204dac5d1f80db2bfaac9c4fb8bc2b3a1
Rinji Le <rinji-le@hotmail.com>
2023-04-20 23:11:25 +0200

    Added classifiers

--- predict.py
+++ predict.py
@@ -12,46 +12,54 @@ import pandas as pd
 from tqdm import tqdm
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
-from sklearn.linear_model import LogisticRegression
+from sklearn.linear_model import LogisticRegression, SGDClassifier, RidgeClassifier
 from sklearn.neural_network import MLPClassifier
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.svm import SVC
 from sklearn.gaussian_process import GaussianProcessClassifier
-from sklearn.tree import DecisionTreeClassifier
+from sklearn.tree import DecisionTreeClassifier, ExtraTreeClassifier
 from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier
-from sklearn.naive_bayes import GaussianNB
+from sklearn.naive_bayes import GaussianNB, BernoulliNB, MultinomialNB, ComplementNB
 from sklearn.multioutput import MultiOutputClassifier
 
 
-def predict(data, targets):
-    X_train, X_test, y_train, y_test = train_test_split(data, targets, test_size=0.2, random_state=42)
+def predict(data, targets, train_size=0.8):
+    # X_train, X_test, y_train, y_test = train_test_split(data, targets, test_size=0.2, random_state=42)
+    # y_test.reset_index(drop=True, inplace=True)
+
+    train_len = int(data.shape[0] * train_size)
+
+    X_train = data.iloc[:train_len]
+    X_test = data.iloc[train_len:]
+    y_train = targets.iloc[:train_len]
+    y_test = targets.iloc[train_len:]
     y_test.reset_index(drop=True, inplace=True)
 
     models = [
         LogisticRegression(max_iter=10**4),
+        SGDClassifier(),
+        RidgeClassifier(),
         GaussianNB(),
+        BernoulliNB(),
+        MultinomialNB(),
+        ComplementNB(),
         KNeighborsClassifier(),
         SVC(),
         GaussianProcessClassifier(),
         DecisionTreeClassifier(),
+        ExtraTreeClassifier(),
         RandomForestClassifier(),
-        MLPClassifier(),
-        AdaBoostClassifier()
+        AdaBoostClassifier(),
+        MLPClassifier()
     ]
     # models = [
-    #     # TODO: random state
     #     RandomForestClassifier(n_estimators=100, max_depth=2, random_state=0),
-    #     # LogisticRegression(random_state=0, max_iter=10**4),
     #     MultiOutputClassifier(GaussianNB()),
     #     KNeighborsClassifier(3),
-    #     # SVC(kernel="linear", C=0.025),
-    #     # SVC(gamma=2, C=1),
-    #     # GaussianProcessClassifier(1.0 * RBF(1.0)),
     #     DecisionTreeClassifier(max_depth=5),
     #     RandomForestClassifier(),
     #     MultiOutputClassifier(MLPClassifier(alpha=1, max_iter=10**4)),
     #     MultiOutputClassifier(AdaBoostClassifier()),
-    #     # QuadraticDiscriminantAnalysis(),
     # ]
 
     accuracies = []
