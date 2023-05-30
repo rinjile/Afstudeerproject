4e5ea37bdbee7cad3421c8893eabf97a7090dc4d
Rinji Le <rinji-le@hotmail.com>
2023-05-30 17:13:40 +0200

    Added command line argument handler

--- plot.py
+++ plot.py
@@ -10,6 +10,7 @@ TODO
 
 import pandas as pd
 import matplotlib.pyplot as plt
+import sys
 import os
 from tqdm import tqdm
 
@@ -20,17 +21,22 @@ def plot(x, y, ci_lower, ci_upper, model):
         plt.plot(x, y[i], label=label, marker="o")
         plt.fill_between(x, ci_lower[i], ci_upper[i], label="95% confidence interval", alpha=0.2)
 
+    # TODO: model naam aanpassen
     plt.title(f"Learning curve for {model} with 5-fold cross-validation")
     plt.xlabel("Number of training samples")
-    plt.ylabel("Accuracy")
+    plt.ylabel("Accuracy")  # TODO: bij regression anders
     plt.legend(loc="best")
     plt.savefig(f"plots/learning_curve_{model}.png", dpi=1000)
     plt.clf()  # Clear the figure
 
 
 def main():
-    files = os.listdir("results")
-    files = [file for file in files if file[:14] == "learning_curve"]
+    if len(sys.argv) > 1:
+        file = sys.argv[1]
+        files = [file.split("/")[1]]
+    else:
+        files = os.listdir("results")
+        files = [file for file in files if file[:14] == "learning_curve"]
 
     for file in tqdm(files, desc="Plotting learning curves"):
         model = file.split("_")[-1].split(".")[0]
