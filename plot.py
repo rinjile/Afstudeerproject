cc0975907405709dbc640c66917750962e639fc9
Rinji Le <rinji-le@hotmail.com>
2023-05-31 02:24:50 +0200

    Changed test -> validation

--- plot.py
+++ plot.py
@@ -16,10 +16,12 @@ from tqdm import tqdm
 
 
 def plot(x, y, ci_lower, ci_upper, model):
-    for i in range(len(y)):
-        label = "Training score" if i == 0 else "Test score"
-        plt.plot(x, y[i], label=label, marker="o")
-        plt.fill_between(x, ci_lower[i], ci_upper[i], label="95% confidence interval", alpha=0.2)
+    # TODO: naar NL vertalen
+
+    plt.plot(x, y[0], label="Training score", marker="o")
+    plt.fill_between(x, ci_lower[0], ci_upper[0], label="95% CI (training)", alpha=0.2)
+    plt.plot(x, y[1], label="Validation score", marker="o")
+    plt.fill_between(x, ci_lower[1], ci_upper[1], label="95% CI (validation)", alpha=0.2)
 
     # TODO: model naam aanpassen
     plt.title(f"Learning curve for {model} with 5-fold cross-validation")
@@ -42,9 +44,9 @@ def main():
         model = file.split("_")[-1].split(".")[0]
         data = pd.read_csv(f"results/{file}")
 
-        plot(data["train_size"], [data["train_mean"], data["test_mean"]],
-             [data["train_ci_lower"], data["test_ci_lower"]],
-             [data["train_ci_upper"], data["test_ci_upper"]], model)
+        plot(data["train_size"], [data["train_mean"], data["validation_mean"]],
+             [data["train_ci_lower"], data["validation_ci_lower"]],
+             [data["train_ci_upper"], data["validation_ci_upper"]], model)
 
 
 if __name__ == "__main__":
