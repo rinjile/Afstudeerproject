bf89ec0184e93d0ea3c13e1d3c8438edaf56f565
Rinji Le <rinji-le@hotmail.com>
2023-06-01 14:27:25 +0200

    Implemented bar plot

--- plot.py
+++ plot.py
@@ -33,7 +33,7 @@ model_names = {
     "GaussianNB": "Gaussian Naive Bayes",
     "BernoulliNB": "Bernoulli Naive Bayes",
     "MultinomialNB": "Multi-nomiaal Naive Bayes",
-    "KNeighborsClassifier": "K-Nearest Neighbors",
+    "KNeighborsClassifier": "K-nearest Neighbors",
     "SGDClassifier": "SGDClassifier",  # TODO
     "SVC": "Support Vector Machine",
     "MLPClassifier": "Neuraal Netwerk",
@@ -42,11 +42,24 @@ model_names = {
 }
 
 
+def plot_bars(data):
+    data = data.sort_values(by="accuracy", ascending=True)
+    models = data["model"]
+    accuracies = data["accuracy"]
+
+    bars = plt.barh([model_names[model] for model in models], accuracies, color="royalblue")
+    plt.bar_label(bars, label_type="center", fmt="%.2f")
+    plt.title("Nauwkeurigheid van verschillende modellen")
+    plt.xlabel("Nauwkeurigheid (%)")
+    plt.tight_layout()
+    plt.savefig("plots/accuracies.png", dpi=1000)
+
+
 def plot_learning_curve(x, y, ci_lower, ci_upper, model):
-    plt.plot(x, y[0], label="Train-score", marker="o")
-    plt.fill_between(x, ci_lower[0], ci_upper[0], label="95% CI (train)", alpha=0.2)
-    plt.plot(x, y[1], label="Validatie-score", marker="o")
-    plt.fill_between(x, ci_lower[1], ci_upper[1], label="95% CI (validatie)", alpha=0.2)
+    plt.plot(x, y[0], label="Train-score", marker="o", color="royalblue")
+    plt.fill_between(x, ci_lower[0], ci_upper[0], label="95% CI (train)", alpha=0.2, color="royalblue")
+    plt.plot(x, y[1], label="Validatie-score", marker="o", color="limegreen")
+    plt.fill_between(x, ci_lower[1], ci_upper[1], label="95% CI (validatie)", alpha=0.2, color="limegreen")
 
     plt.title(f"{model_names[model]}: leercurve met 5-voudige kruisvalidatie")
     plt.xlabel("Trainset grootte")
@@ -62,20 +75,33 @@ def plot_learning_curve(x, y, ci_lower, ci_upper, model):
 
 
 def main():
-    if len(sys.argv) > 1:
-        file = sys.argv[1]
-        files = [file.split("/")[1]]
-    else:
-        files = os.listdir("results")
-        files = [file for file in files if file[:14] == "learning_curve"]
+    if len(sys.argv) < 2:
+        print("Usage: python plot.py --bar <file>")
+        print("       python plot.py --lc [file]")
+        return
 
-    for file in tqdm(files, desc="Plotting learning curves"):
-        model = file.split("_")[-1].split(".")[0]
-        data = pd.read_csv(f"results/{file}")
+    if sys.argv[1] == "--bar":
+        if len(sys.argv) < 3:
+            print("Usage: python plot.py --bar <file>")
+            return
+        else:
+            data = pd.read_csv(sys.argv[2])
+            plot_bars(data)
+    elif sys.argv[1] == "--lc":
+        if len(sys.argv) > 2:
+            file = sys.argv[2]
+            files = [file.split("/")[1]]
+        else:
+            files = os.listdir("results")
+            files = [file for file in files if file[:14] == "learning_curve"]
 
-        plot_learning_curve(data["train_size"], [data["train_mean"], data["validation_mean"]],
-                            [data["train_ci_lower"], data["validation_ci_lower"]],
-                            [data["train_ci_upper"], data["validation_ci_upper"]], model)
+        for file in tqdm(files, desc="Plotting learning curves"):
+            model = file.split("_")[-1].split(".")[0]
+            data = pd.read_csv(f"results/{file}")
+
+            plot_learning_curve(data["train_size"], [data["train_mean"], data["validation_mean"]],
+                                [data["train_ci_lower"], data["validation_ci_lower"]],
+                                [data["train_ci_upper"], data["validation_ci_upper"]], model)
 
 
 if __name__ == "__main__":
