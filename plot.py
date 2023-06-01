a2328c0dc127e5fe8ae6010dd784d1a97423ac28
Rinji Le <rinji-le@hotmail.com>
2023-06-01 12:38:50 +0200

    ylabel text for classifier and regressor

--- plot.py
+++ plot.py
@@ -14,6 +14,19 @@ import sys
 import os
 from tqdm import tqdm
 
+classifiers = [
+    "LogisticRegression",
+    "GaussianNB",
+    "BernoulliNB",
+    "MultinomialNB",
+    "KNeighborsClassifier",
+    "SGDClassifier",
+    "SVC",
+    "MLPClassifier",
+    "DecisionTreeClassifier",
+    "RandomForestClassifier"
+]
+
 # TODO: regression
 model_names = {
     "LogisticRegression": "Logistische Regressie",
@@ -25,20 +38,24 @@ model_names = {
     "SVC": "Support Vector Machine",
     "MLPClassifier": "Neuraal Netwerk",
     "DecisionTreeClassifier": "Decision Tree",
-    "RandomForestClassifier": "Random Forest",
+    "RandomForestClassifier": "Random Forest"
 }
 
 
-def plot(x, y, ci_lower, ci_upper, model):
+def plot_learning_curve(x, y, ci_lower, ci_upper, model):
     plt.plot(x, y[0], label="Train-score", marker="o")
     plt.fill_between(x, ci_lower[0], ci_upper[0], label="95% CI (train)", alpha=0.2)
     plt.plot(x, y[1], label="Validatie-score", marker="o")
     plt.fill_between(x, ci_lower[1], ci_upper[1], label="95% CI (validatie)", alpha=0.2)
 
-    # TODO: model naam aanpassen
     plt.title(f"{model_names[model]}: leercurve met 5-voudige kruisvalidatie")
     plt.xlabel("Trainset grootte")
-    plt.ylabel("Nauwkeurigheid")  # TODO: bij regression anders
+
+    if model in classifiers:
+        plt.ylabel("Nauwkeurigheid")
+    else:
+        plt.ylabel("Gemiddelde absolute fout (negatie)")
+
     plt.legend(loc="best")
     plt.savefig(f"plots/learning_curve_{model}.png", dpi=1000)
     plt.clf()  # Clear the figure
@@ -56,9 +73,9 @@ def main():
         model = file.split("_")[-1].split(".")[0]
         data = pd.read_csv(f"results/{file}")
 
-        plot(data["train_size"], [data["train_mean"], data["validation_mean"]],
-             [data["train_ci_lower"], data["validation_ci_lower"]],
-             [data["train_ci_upper"], data["validation_ci_upper"]], model)
+        plot_learning_curve(data["train_size"], [data["train_mean"], data["validation_mean"]],
+                            [data["train_ci_lower"], data["validation_ci_lower"]],
+                            [data["train_ci_upper"], data["validation_ci_upper"]], model)
 
 
 if __name__ == "__main__":
