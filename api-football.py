ec20ccc487d50e109b9cdf64dab0b509cb32f1cb
Rinji Le <rinji-le@hotmail.com>
2023-06-04 01:58:20 +0200

    Removed get_players_stats and get_h2h

--- api-football.py
+++ api-football.py
@@ -57,26 +57,6 @@ def check_file_exists(filename):
                 sys.exit(0)
 
 
-def date_ago(date, years=0, months=0):
-    """
-    Calculates the date a certain amount of years and months ago.
-
-    :param date: Date (str).
-    :param years: Amount of years (int).
-    :param months: Amount of months (int).
-    :return: New date (str).
-    """
-    new_year = int(date[:4]) - years
-
-    while int(date[5:7]) - months < 1:
-        new_year -= 1
-        months -= 12
-
-    new_month = int(date[5:7]) - months
-
-    return f"{new_year}-{new_month:02d}-{date[8:10]}"
-
-
 def request(path, query, sleep=0.2):
     """
     Sends a get request to the API. The API has a limit of 300 requests per
@@ -88,7 +68,6 @@ def request(path, query, sleep=0.2):
     :return: Response from the API (requests.Response).
     """
     response = requests.request("GET", URL + path, headers=HEADERS, params=query)
-    # sleep = 6  # TODO: verwijderen
     time.sleep(sleep)  # Prevent exceeding API's requests/min limit
     return response
 
@@ -143,88 +122,8 @@ def get_fixture_stats():
     return stats
 
 
-def get_fixture_player_stats():
-    player_stats = pd.DataFrame()
-
-    fixtures = pd.read_csv("data/fixtures.csv", low_memory=False)
-    fixture_ids = fixtures["fixture.id"]
-
-    for idx in tqdm(fixture_ids, desc="Fixture player stats"):
-        query = {"fixture": idx}
-        response = request("/fixtures/players", query)
-
-        if not response:
-            ERROR.append(f"Error (fixture {idx}): No response.")
-            continue
-
-        response = response.json()
-
-        if response["errors"]:
-            print(f"Error (fixture {idx}): {response['errors']}")
-            break
-        else:
-            new_player_stats = pd.json_normalize(response,
-                                                 record_path=["response", "players", "statistics"],
-                                                 meta=[["response", "players", "player", "id"],
-                                                       ["response", "players", "player", "name"],
-                                                       ["response", "team"]])
-            if new_player_stats.shape[0] == 0:
-                ERROR.append(f"Error (fixture {idx}): No player stats found.")
-                continue
-
-            new_player_stats.insert(0, "fixture.id", idx)
-            new_player_stats["games.captain"] = new_player_stats["games.captain"].astype(bool)
-            new_player_stats["games.substitute"] = new_player_stats["games.substitute"].astype(bool)
-
-            player_stats = pd.concat([player_stats, new_player_stats])
-
-    # Convert dict in "response.team" column to separate columns
-    player_stats["team.id"] = player_stats["response.team"].apply(lambda x: x["id"])
-    player_stats["team.name"] = player_stats["response.team"].apply(lambda x: x["name"])
-    player_stats["team.update"] = player_stats["response.team"].apply(lambda x: x["update"])
-    player_stats.drop(columns=["response.team"], inplace=True)
-
-    # Move last 4 columns after the first column
-    cols = player_stats.columns.tolist()
-    cols = cols[:1] + cols[-4:] + cols[1:-4]
-    player_stats = player_stats[cols]
-
-    player_stats.columns = player_stats.columns.str.replace("response.players.", "", regex=False)
-
-    return player_stats
-
-
-def get_fixture_h2h():
-    h2h = pd.DataFrame()
-
-    fixtures = pd.read_csv("data/fixtures.csv", low_memory=False)
-    fixtures = fixtures[["fixture.id", "teams.home.id", "teams.away.id", "fixture.date"]]
-
-    for (idx, home, away, date) in tqdm(fixtures.itertuples(index=False), desc="Fixture h2h"):
-        date = date[:10]  # Only use the date, not the time
-        query = {
-            "h2h": f"{home}-{away}",
-            "from": date_ago(date, years=1),  # TODO: check
-            "to": date,
-            "status": "FT"
-        }
-        response = request("/fixtures/statistics", query).json()
-
-        if response["errors"]:
-            print(f"Error (fixture {idx}): {response['errors']}")
-            break
-        else:
-            # TODO
-            new_h2h = pd.json_normalize(response,
-                                        record_path=[],
-                                        meta=[])
-            h2h = pd.concat([h2h, new_h2h])
-
-    return h2h
-
-
 def main():
-    usage_message = "Usage: python3 api-football.py -1/-2/-3 <file name>"
+    usage_message = "Usage: python3 api-football.py --fixtures/--stats <file name>"
 
     if len(sys.argv) != 3:
         print(usage_message)
@@ -235,12 +134,10 @@ def main():
 
     ids = read_ids("data/ids.csv")
 
-    if sys.argv[1] == "-1":
+    if sys.argv[1] == "--fixtures":
         data = get_fixtures(ids)
-    elif sys.argv[1] == "-2":
+    elif sys.argv[1] == "--stats":
         data = get_fixture_stats()
-    elif sys.argv[1] == "-3":
-        data = get_fixture_player_stats()
     else:
         print(usage_message)
         sys.exit(1)
